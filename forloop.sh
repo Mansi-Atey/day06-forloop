6818a3ae32b91978acf2b0362cbeb84b74af9bbb
unknown <you@example.com>
2021-02-20 00:35:54 +0530

    Addded: code for prime number

--- forloop.sh
+++ forloop.sh
@@ -1,9 +1,13 @@
  #!/bin/bash
- echo "Enter n value"
- read n
-temp=0
-for ((i=1;i<=n;i++))
- do
-   temp=$(($temp+1/$i))
- done
-echo $temp
+echo "Enter a number"
+read num
+num=$num
+for((i=2; i<=num/2; i++))
+do
+  if [ $((num%i)) -eq 0 ]
+  then
+    echo "$num is not a prime number."
+    exit
+  fi
+done
+echo "$num is a prime number."
